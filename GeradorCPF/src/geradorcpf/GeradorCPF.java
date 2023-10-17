8058c3d1b1cbda21d1f18d29abcb1b241c23230d
V E A <V E A@DESKTOP-3015RRM>
2023-10-17 19:55:02 -0300

    versão 1.2 Melhoria no código deixando mais limpo

--- GeradorCPF/src/geradorcpf/GeradorCPF.java
+++ GeradorCPF/src/geradorcpf/GeradorCPF.java
@@ -3,7 +3,7 @@ package geradorcpf;
 /**
  * @author Vinícius Desenv
  * Gerador de CPF validos
- * Versão 1.0
+ * Versão 1.2
  */
 
 import java.util.Random;
@@ -14,41 +14,26 @@ public class GeradorCPF {
    
         Random random = new Random();                                     
         int[] cpf = new int[11]; // array para armazenar os dígitos do CPF
-        
+                
         for (int i = 0; i < 9; i++) {
             int numeroAleatorio = random.nextInt(10); // Gera um número inteiro aleatório entre 0 e 9 
             cpf[i] = numeroAleatorio;
         }
-    
-        int primeiro = 10 * cpf[0];
-        int segundo = 9 * cpf [1];
-        int terceiro = 8 * cpf[2];
-        int quarto = 7 * cpf[3];
-        int quinto = 6 * cpf[4];
-        int sexto = 5 * cpf[5];
-        int setimo = 4 * cpf[6];
-        int oitavo = 3 * cpf[7];
-        int nono = 2 * cpf[8];
         
-        int soma = primeiro + segundo + terceiro + quarto + quinto + sexto + setimo + oitavo + nono;
+            int soma = 0;        
+            for(int a = 0; a < 9; a++){
+            soma += cpf[a] * (10 - a); // soma recebe a multiplicação e depois soma dos números aleatorios com os definidos na regra             
+            }
+         
         int numeroVerificado1 = soma % 11; // Décimo numero do CPF
-        
         cpf [9] = numeroVerificado1 < 2 ? 0 : (11 - numeroVerificado1); // Verificar se número é menor que 2
-                 
-        int primeiro2 = 11 * cpf[0];
-        int segundo2 = 10 * cpf [1];
-        int terceiro2 = 9 * cpf[2];
-        int quarto2 = 8 * cpf[3];
-        int quinto2 = 7 * cpf[4];
-        int sexto2 = 6 * cpf[5];
-        int setimo2 = 5 * cpf[6];
-        int oitavo2 = 4 * cpf[7];
-        int nono2 = 3 * cpf[8];
-        int decimo = 2 * cpf[9];
-                
-        int calc = primeiro2 + segundo2 + terceiro2 + quarto2 + quinto2 + sexto2 + setimo2 + oitavo2 + nono2 + decimo;
-        int numeroVerificado2 = calc % 11; // Décimo primeironumero do CPF
+         
+            int calc = 0;        
+            for(int a = 0; a < 10; a++){
+                calc += cpf[a] * (10 - a); // soma recebe a multiplicação e depois soma dos números aleatorios com os definidos na regra   
+            }
         
+        int numeroVerificado2 = calc % 11; // Décimo primeironumero do CPF        
         cpf [10] = numeroVerificado2 < 2 ? 0 : (11 - numeroVerificado2); // Verificar se número é menor que 2
         
         imprimirCPF(cpf); // Metodo imprime CPF
@@ -63,4 +48,3 @@ public class GeradorCPF {
                 cpf[9] * 10 + cpf[10]);
     }
 }
-
